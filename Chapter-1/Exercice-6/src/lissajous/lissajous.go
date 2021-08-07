78cc8e69f9bee1b10e86d497451b6dc9c1daec47
pperret <perret.ph@gmail.com>
2021-08-07 11:17:05 +0200

    Refactoring in order to obtain a more generic code

--- Chapter-1/Exercice-6/src/lissajous/lissajous.go
+++ Chapter-1/Exercice-6/src/lissajous/lissajous.go
@@ -12,38 +12,31 @@ import (
 
 var palette = []color.Color{color.White, color.RGBA{0xff, 0, 0, 0xff}, color.RGBA{0, 0xff, 0, 0xff}, color.RGBA{0, 0, 0xff, 0xff}}
 
-const (
-	whiteIndex = 0 // first color in palette
-	redIndex   = 1 // next color in palette
-	greenIndex = 2
-	blueIndex  = 3
-)
-
 func main() {
 	lissajous(os.Stdout)
 }
+
 func lissajous(out io.Writer) {
 	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 300   // image canvas covers [size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-		colors  = 3     // number of colors
+		cycles      = 5     // number of complete x oscillator revolutions
+		res         = 0.001 // angular resolution
+		size        = 300   // image canvas covers [size..+size]
+		nframes     = 64    // number of animation frames
+		delay       = 8     // delay between frames in 10ms units
+		phaseOffset = 1.0   // Phase offet between colors
 	)
-	phaseOffset := []float64{0.0, 1.0, 2.0} // phase difference for each color
-	freq := rand.Float64() * 3.0            // relative frequency of y oscillator
+	colors := len(palette) - 1   // number of colors
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
 	anim := gif.GIF{LoopCount: nframes}
 	phase := 0.0
 	for i := 0; i < nframes; i++ {
 		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
 		img := image.NewPaletted(rect, palette)
 		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			var c uint8
-			for c = 0; c < colors; c++ {
+			for c := 0; c < colors; c++ {
 				x := math.Sin(t)
-				y := math.Sin(t*freq + phase + phaseOffset[c])
-				img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), c+1)
+				y := math.Sin(t*freq + phase + phaseOffset*float64(c))
+				img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), uint8(c+1))
 			}
 		}
 		phase += 0.1
