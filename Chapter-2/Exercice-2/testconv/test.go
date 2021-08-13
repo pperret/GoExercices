4ebcdd9ff222a1e77f5be1f6820a764168bd5895
pperret <perret.ph@gmail.com>
2021-08-13 09:17:45 +0200

    Change of a displayed string

--- Chapter-2/Exercice-2/testconv/test.go
+++ Chapter-2/Exercice-2/testconv/test.go
@@ -1,3 +1,4 @@
+// Test program for unit (temperature, length, weight) conversions
 package main
 
 import (
@@ -39,7 +40,7 @@ func main() {
 		for _, arg := range os.Args[1:] {
 			t, err := strconv.ParseFloat(arg, 64)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+				fmt.Fprintf(os.Stderr, "Invalid parameter: %v\n", err)
 				os.Exit(1)
 			}
 			conv(t)
