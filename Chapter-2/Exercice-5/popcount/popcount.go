fcb53b81bf7427a32f0a952e687d424ebb12111f
pperret <perret.ph@gmail.com>
2021-08-14 06:46:36 +0200

    Adding the "Divide and Conquer Strategy" implementation

--- Chapter-2/Exercice-5/popcount/popcount.go
+++ Chapter-2/Exercice-5/popcount/popcount.go
@@ -30,3 +30,16 @@ func PopCount2(x uint64) int {
 	}
 	return n
 }
+
+// PopCount3 if the "Divide and Conquer Strategy" version of bits counting
+func PopCount3(x uint64) int {
+	const k1 = 0x5555555555555555  /*  -1/3   */
+	const k2 = 0x3333333333333333  /*  -1/5   */
+	const k4 = 0x0f0f0f0f0f0f0f0f  /*  -1/17  */
+	const kf = 0x0101010101010101  /*  -1/255 */
+	x = x - ((x >> 1) & k1)        /* put count of each 2 bits into those 2 bits */
+	x = (x & k2) + ((x >> 2) & k2) /* put count of each 4 bits into those 4 bits */
+	x = (x + (x >> 4)) & k4        /* put count of each 8 bits into those 8 bits */
+	x = (x * kf) >> 56             /* returns 8 most significant bits of x + (x<<8) + (x<<16) + (x<<24) + ...  */
+	return int(x)
+}
