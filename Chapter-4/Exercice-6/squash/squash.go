3882a441ebc956cd86f5b601a44da2a5fceb92eb
pperret <perret.ph@gmail.com>
2021-08-23 07:48:55 +0200

    Rename the program

--- Chapter-4/Exercice-6/squash/squash.go
+++ Chapter-4/Exercice-6/squash/squash.go
@@ -8,27 +8,27 @@ import (
 )
 
 func main() {
-	str1 := "Bonjour    à tous les éléments  !!      !!"
-	tab1 := []byte(str1)
+	str1 := "	 Bonjour    à tous les éléments  !!      !! 	 "
+	fmt.Printf("%q\n", str1)
 
-	tab2 := unique(tab1)
+	tab1 := []byte(str1)
+	fmt.Printf("Before: % x\n", tab1)
+
+	tab2 := squash(tab1)
+	fmt.Printf("After: % x\n", tab2)
 
 	str2 := string(tab2)
-
-	fmt.Println(str1)
-	fmt.Println(tab1)
-	fmt.Println(tab2)
-	fmt.Println(str2)
+	fmt.Printf("%q\n", str2)
 }
 
-// Purge adjacent spaces
-func unique(s []byte) []byte {
+// Squashes adjacent spaces
+func squash(s []byte) []byte {
 	to := 0
 	space := false
 	for from := 0; from < len(s); {
 		r, l := utf8.DecodeRune(s[from:])
 		if unicode.IsSpace(r) {
-			if space == false {
+			if !space {
 				s[to] = ' '
 				to++
 			}
