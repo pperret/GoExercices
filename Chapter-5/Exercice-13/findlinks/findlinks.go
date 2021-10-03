16a422ee71011dd7c70272db9873e942dbc374be
pperret <perret.ph@gmail.com>
2021-10-03 09:39:58 +0200

    Code formatting, error management and support of invalid certificates

--- Chapter-5/Exercice-13/findlinks/findlinks.go
+++ Chapter-5/Exercice-13/findlinks/findlinks.go
@@ -2,11 +2,11 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"net/url"
+	neturl "net/url"
 	"os"
 	"path/filepath"
 
@@ -17,13 +17,17 @@ import (
 func main() {
 	// Crawl the web breadth-first,
 	// starting from the command-line arguments.
-	breadthFirst(crawl, os.Args[1:])
+	err := breadthFirst(crawl, os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
 
 // breadthFirst calls f for each item in the worklist.
 // Any items returned by f are added to the worklist.
 // f is called at most once for each item.
-func breadthFirst(f func(item string) []string, worklist []string) {
+func breadthFirst(f func(item string) ([]string, error), worklist []string) error {
 	seen := make(map[string]bool)
 	for len(worklist) > 0 {
 		items := worklist
@@ -31,49 +35,60 @@ func breadthFirst(f func(item string) []string, worklist []string) {
 		for _, item := range items {
 			if !seen[item] {
 				seen[item] = true
-				links := f(item)
-				if len(links) > 0 {
-					parsedURL, err := url.Parse(item)
-					if err != nil {
-						log.Print(err)
-					}
-					// Only link of current domain are considered
-					for _, link := range links {
-						parsedLink, err := url.Parse(link)
-						if err != nil {
-							log.Print(err)
-							continue
-						}
-						if parsedLink.Scheme != parsedURL.Scheme || len(parsedLink.Scheme) == 0 {
-							continue
-						}
-						if parsedLink.Host != parsedURL.Host || len(parsedLink.Host) == 0 {
-							continue
-						}
-						worklist = append(worklist, link)
-					}
+				links, err := f(item)
+				if err != nil {
+					return err
 				}
+				worklist = append(worklist, links...)
 			}
 		}
 	}
+	return nil
 }
 
 // crawl gets the list of links in the HTML page specified by url parameter
-func crawl(url string) []string {
+func crawl(url string) ([]string, error) {
+
+	// Print the current URL path
 	fmt.Println(url)
+
 	// Save the content
 	err := saveContent(url)
 	if err != nil {
-		log.Print(err)
-		return nil
+		fmt.Fprintf(os.Stderr, "Unable to save %v: %v\n", url, err)
+		return []string{}, nil // Error is ignored
 	}
 
 	// Extract the links from the URL
-	list, err := Extract(url)
+	links, err := Extract(url)
 	if err != nil {
-		log.Print(err)
+		return nil, err
 	}
-	return list
+
+	// Retain only local URLs
+	var worklist []string
+	if len(links) > 0 {
+		parsedCurrentURL, err := neturl.Parse(url)
+		if err != nil {
+			return nil, err
+		}
+		// Only link of current domain are considered
+		for _, link := range links {
+			parsedLink, err := neturl.Parse(link)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Invalid URL: %v (%v)\n", link, err)
+				continue
+			}
+			if parsedLink.Scheme != parsedCurrentURL.Scheme || len(parsedLink.Scheme) == 0 {
+				continue
+			}
+			if parsedLink.Host != parsedCurrentURL.Host || len(parsedLink.Host) == 0 {
+				continue
+			}
+			worklist = append(worklist, link)
+		}
+	}
+	return worklist, nil
 }
 
 // Extract makes an HTTP GET request to the specified URL,
@@ -81,7 +96,10 @@ func crawl(url string) []string {
 // the links in the HTML document.
 func Extract(url string) ([]string, error) {
 	// Get the URL content
-	resp, err := http.Get(url)
+	customTransport := http.DefaultTransport.(*http.Transport).Clone()
+	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	client := &http.Client{Transport: customTransport}
+	resp, err := client.Get(url)
 	if err != nil {
 		return nil, err
 	}
@@ -115,19 +133,22 @@ func Extract(url string) ([]string, error) {
 }
 
 // saveContent saves the content of the URL
-func saveContent(htmlURL string) error {
+func saveContent(url string) error {
 	// Get the URL content
-	resp, err := http.Get(htmlURL)
+	customTransport := http.DefaultTransport.(*http.Transport).Clone()
+	customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	client := &http.Client{Transport: customTransport}
+	resp, err := client.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("getting %s: %s", htmlURL, resp.Status)
+		return fmt.Errorf("getting %s: %s", url, resp.Status)
 	}
 
 	// Parse the URL
-	parsedURL, err := url.Parse(htmlURL)
+	parsedURL, err := neturl.Parse(url)
 	if err != nil {
 		return err
 	}
@@ -135,10 +156,10 @@ func saveContent(htmlURL string) error {
 	// Build the file name
 	path := filepath.FromSlash(parsedURL.Path)
 	if len(path) == 0 {
-		path = "default.html"
+		path = "index.html"
 	} else {
 		if path[len(path)-1] == os.PathSeparator {
-			path = filepath.Join(path, "default.html")
+			path = filepath.Join(path, "index.html")
 		}
 	}
 	path = filepath.Join(parsedURL.Host, path)
