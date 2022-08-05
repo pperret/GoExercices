94100aaf4d357e66f5aecf3ed924cbb6fade19dc
pperret <perret.ph@gmail.com>
2022-08-05 07:45:57 +0200

    Addition of exercice 10.4

---
+++ Chapter-12/Exercice-4/sexpr/encode.go
@@ -0,0 +1,87 @@
+// sexpr is a encoding notation using S-expressions, the syntax of Lisp
+package sexpr
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+)
+
+// Marshal encodes a Go value in S-expression form.
+func Marshal(v interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := encode(&buf, reflect.ValueOf(v), 0); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+// encode writes to buf an S-expression representation of v.
+func encode(buf *bytes.Buffer, v reflect.Value, indentation int) error {
+	switch v.Kind() {
+	case reflect.Invalid:
+		buf.WriteString("nil")
+
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		fmt.Fprintf(buf, "%d", v.Int())
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		fmt.Fprintf(buf, "%d", v.Uint())
+
+	case reflect.String:
+		fmt.Fprintf(buf, "%q", v.String())
+
+	case reflect.Ptr:
+		return encode(buf, v.Elem(), indentation)
+
+	case reflect.Array, reflect.Slice: // (value ...)
+		buf.WriteByte('(')
+		for i := 0; i < v.Len(); i++ {
+			if i > 0 {
+				fmt.Fprintf(buf, "\n%*s", indentation+1, "")
+			}
+			if err := encode(buf, v.Index(i), indentation+1); err != nil {
+				return err
+			}
+		}
+		buf.WriteByte(')')
+
+	case reflect.Struct: // ((name value) ...)
+		buf.WriteByte('(')
+		for i := 0; i < v.NumField(); i++ {
+			if i > 0 {
+				fmt.Fprintf(buf, "\n%*s", indentation+1, "")
+			}
+			fmt.Fprintf(buf, "(%s ", v.Type().Field(i).Name)
+			if err := encode(buf, v.Field(i), indentation+1+1+len(v.Type().Field(i).Name)+1); err != nil {
+				return err
+			}
+			buf.WriteByte(')')
+		}
+		buf.WriteByte(')')
+
+	case reflect.Map: // ((key value) ...)
+		buf.WriteByte('(')
+		for i, key := range v.MapKeys() {
+			if i > 0 {
+				fmt.Fprintf(buf, "\n%*s", indentation+1, "")
+			}
+			buf.WriteByte('(')
+			if err := encode(buf, key, indentation+1+1); err != nil {
+				return err
+			}
+			buf.WriteByte(' ')
+			if err := encode(buf, v.MapIndex(key), indentation+1+1); err != nil {
+				return err
+			}
+			buf.WriteByte(')')
+		}
+		buf.WriteByte(')')
+
+	default: // chan, func
+		return fmt.Errorf("unsupported type: %s", v.Type())
+	}
+	return nil
+}
